7318e70577c228acb0e4d43d50afee9247b58d88
Andre Correia <andre.correia@farfetch.com>
2018-06-28 12:37:56 +0100

    add just the initial structure (copy)

---
+++ src/GenericHostSample/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GenericHostSample
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("hostsettings.json", optional: true);
+                    configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+                    configHost.AddCommandLine(args);
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.AddJsonFile("appsettings.json");
+                    configApp.AddJsonFile(
+                        $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                        optional: true);
+                    configApp.AddEnvironmentVariables(prefix: "PREFIX_");
+                    configApp.AddCommandLine(args);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.ConfigureExternalServices();
+                    services.AddLogging();
+                    services.AddHostedService<LifetimeEventsHostedService>();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            await host.RunAsync();
+        }
+
+        private static void ConfigureExternalServices(this IServiceCollection services)
+        {
+            // to add servies
+        }
+    }
+}
